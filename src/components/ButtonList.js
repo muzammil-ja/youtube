c371aa4fe730056e3891a96de8e20d834fa61b88
Muzammil <ma7585401@gmail.com>
2026-02-13 22:00:17 +0530

    Initial commit

---
+++ src/components/ButtonList.js
@@ -0,0 +1,21 @@
+import Button from "./Button";
+
+
+const ButtonList = ()=>{
+    return (
+        <div className="flex">
+         <Button name="All"/>
+         <Button name="Gaming"/>
+         <Button name="Songs"/>
+         <Button name="Live"/>
+         <Button name="Cricket"/>
+         <Button name="Soccer"/>
+         <Button name="News"/>
+          <Button name="Cooking"/>
+          <Button name="Thoughts"/>
+          <Button name="Mixes"/>
+          <Button name="Study skills"/>
+        </div>
+    )
+}
+export default ButtonList;
---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+const GOOGLE_API_KEY="AIzaSyAv7HCOEpCabF7AHNfATazAzrAp-nZ4yE8"
+
+export const OFFSET_LIVE_CHAT=25
+
+export const YOUTUBE_VIDEO_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet,statistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + GOOGLE_API_KEY
+   
+export const YOUTUBE_SEARCH_API=
+"https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
